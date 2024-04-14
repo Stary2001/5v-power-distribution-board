405c505d176253b3694c6778b3699d755c3363cd
Chloe Bethel <chloe@9net.org>
2024-04-14 17:05:51 +0100

    e

---
+++ firmware/mongoose_custom.h
@@ -0,0 +1,14 @@
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+#define MG_ARCH MG_ARCH_CUSTOM
+#define MG_ENABLE_SOCKET 0
+#define MG_ENABLE_TCPIP 1
+#define MG_ENABLE_DRIVER_W5500 1
+#define mkdir(a, b) (-1)
+#define MG_IO_SIZE 512
